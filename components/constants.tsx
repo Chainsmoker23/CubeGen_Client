56fdd183926d33e595b9266d28eb35f4b57ec130
Divesh Sarkar <141997073+Chainsmoker23@users.noreply.github.com>
2026-01-15 00:03:10 +0530

    Update constants.tsx

--- components/constants.tsx
+++ components/constants.tsx
@@ -245,5 +245,5 @@ export const AVATARS = [
     { name: 'Captain', svg: `<svg viewBox="0 0 24 24"><path fill="#059669" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM12 5c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm-4 8c0-1.1.9-2 2-2h4c1.1 0 2 .9 2 2v1c0 1.1-.9 2-2 2H10c-1.1 0-2-.9-2-2v-1z"/></svg>` },
 ];
 
-export const FREE_GENERATION_LIMIT = 10;
-export const HOBBYIST_GENERATION_LIMIT = 50;
+export const FREE_GENERATION_LIMIT = 3;
+export const HOBBYIST_GENERATION_LIMIT = 20;
